1f9cd5457b8830305316036e964498402b886100
Daniel-Georgiev <daniko91@mail.bg>
2014-12-05 17:22:18 +0200

    HomeWorks

---
+++ PHPFlow/04.FindPrimesInRange/FindPrimesInRange.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Find Primes</title>
+</head>
+<body>
+    <form>
+        <label for="start">Starting Index: </label>
+        <input type="text" id="start" name="start"/>
+        <label for="end">End: </label>
+        <input type="text" id="end" name="end"/>
+        <input type="submit"/>
+    </form>
+</body>
+</html>
